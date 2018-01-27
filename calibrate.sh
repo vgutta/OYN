009e9ee8c79c60f49e87bbba5b2558a78bd3b9fc
Collin Clark <crclark96@gmail.com>
2018-01-27 13:46:36 -0500

    simple calibration script

--- calibrate.sh
+++ calibrate.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
 
-./myo_collect | tee time.pos | grep ORT
+REST=rest.pos
+
+echo "enter filename: "
+read POS
+
+echo "press enter once in position "
+read
+
+./myo_collect > $POS &
+PID=$!
+sleep 1 && echo -n "calibrating"
+
+for N in 1 .. 4
+do
+    sleep 1 && echo -n "."
+done
+
+echo
+
+sleep 1 && kill $PID | /dev/null
+
+
+echo "press enter when in resting position"
+read
+
+./myo_collect > $REST &
+PID=$!
+sleep 1 && echo -n "calibrating"
+
+for N in 1 .. 4
+do
+    sleep 1 && echo -n "."
+done
+
+echo
+
+sleep 1 && kill $PID | /dev/null
 
