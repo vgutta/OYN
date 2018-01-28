2768135d0402e652b5bc592317e889088ac0d5c1
Collin Clark <crclark96@gmail.com>
2018-01-28 03:44:16 -0500

    silence killed processes

--- calibrate.sh
+++ calibrate.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-echo "enter filename for data: "
-read POS 				#calibration data is written to POS file
-
-
-for index in 1 .. 5
+for POS in dab.pos please.pos thanks.pos hi.pos time.pos
 do
-	echo "press enter once in position "
-	read
-
-	#writes to file
-        if [ $index == 1 ]
-        then
-	    ./myo_collect > $POS &
-        else
-            ./myo_collect >> $POS &
-        fi
-        
-
-            
-	PID=$!
-	sleep 0.5
-	echo -n "calibrating"
-
-	for N in 1 .. 4
+    echo $POS
+	for index in 1 .. 5
 	do
-	    sleep 0.5 && echo -n "."
+		echo "press enter once in position "
+		read
+	
+		#writes to file
+	        if [ $index == 1 ]
+	        then
+		    ./myo_collect > $POS &
+	        else
+	            ./myo_collect >> $POS &
+	        fi
+	        
+	
+	            
+		PID=$!
+		sleep 0.5
+		echo -n "calibrating"
+	
+		for N in 1 .. 4
+		do
+		    sleep 0.5 && echo -n "."
+		done
+                echo
+                
+		sleep 0.5
+		kill $PID
+                wait $PID 2>/dev/null
+	
+		echo "rest your arm"
+		sleep 3
+		echo
 	done
 
-	sleep 0.5
-	kill $PID | /dev/null
-
-	echo "rest your arm"
-	sleep 3
-	echo
 done
